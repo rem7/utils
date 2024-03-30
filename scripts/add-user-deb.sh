935c20399fe72dbd0510cdacec7c419e7824545e
Yanko Bolanos <y@rem7.com>
2024-03-29 20:38:20 -0700

    using rem7 as user

--- scripts/add-user-deb.sh
+++ scripts/add-user-deb.sh
@@ -2,11 +2,11 @@
 #
 # bash <(curl -s https://raw.githubusercontent.com/rem7/utils/master/scripts/add-user-deb.sh yanko)
 
-adduser --disabled-password --gecos "" -q $1
-usermod -a -G admin $1
-sudo -u $1 mkdir ~$1/.ssh
-sudo -u $1 chmod 755 ~$1/.ssh
-sudo -u $1 gpg --keyserver keyserver.ubuntu.com --recv-keys 047BC46E1CE0AA53927D92002A1633C650997C84
-sudo -u $1 bash -c 'curl -L https://yanko.pub | gpg --output - --verify > ~$1/.ssh/authorized_keys'
+adduser --disabled-password --gecos "" -q rem7
+usermod -a -G admin rem7
+sudo -u rem7 mkdir ~rem7/.ssh
+sudo -u rem7 chmod 755 ~rem7/.ssh
+sudo -u rem7 gpg --keyserver keyserver.ubuntu.com --recv-keys 047BC46E1CE0AA53927D92002A1633C650997C84
+sudo -u rem7 bash -c 'curl -L https://yanko.pub | gpg --output - --verify > ~rem7/.ssh/authorized_keys'
 bash -c 'echo "%admin ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/91-admin'
 chmod 0440 /etc/sudoers.d/91-admin
